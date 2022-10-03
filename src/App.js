0164927d03a04ca0a4f8b86c85727b0bbd118913
jucondef <juan.conde.f@ab-inbev.com>
2022-10-03 17:08:57 -0500

    endpoints launch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
-
+import * as API from  './services/launches';
 import './App.css';
+import {useState, useEffect} from 'react';
 // import Greeting from './components/pure/greeting';
 // import GreetingF from './components/pure/greetingF';
 // import TaskComponent from './components/container/task_list';
@@ -8,9 +9,15 @@ import './App.css';
 // import Ejm1 from './hooks/ejm1';
 //  import Ejm2 from './hooks/ejm2';
 // import ComponentWithContext from './hooks/ejm3';
-import Ejm4 from './hooks/ejm4';
+// import Ejm4 from './hooks/ejm4';
 
 function App() {
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    API.getAllLaunches().then(setLaunches);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,11 +28,17 @@ function App() {
           {/* <Ejm1></Ejm1> */}
           {/* <Ejm2></Ejm2> */}
           {/* <ComponentWithContext></ComponentWithContext> */}
-          <Ejm4 name="pepe">
+          {/* <Ejm4 name="pepe">
             <h3>
               Contenido del props.children
             </h3>
-          </Ejm4>
+          </Ejm4> */}
+          <ul>
+            {/* {launches.flight_number} */}
+            {launches.map(launch => (
+             <li key={launch.flight_number}>{launch.mission_name} ({launch.launch_year})</li>
+            ))}
+          </ul>
       </header>
     </div>
   );
